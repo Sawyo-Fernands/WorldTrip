2ec49973b872659580d74e2768fa1c1628da18ef
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-04-11 16:22:12 -0300

    Update index.jsx

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,7 +5,7 @@ import { Flex, Image } from "@chakra-ui/react";
 export function Header(){
     return(
         <Flex width={'100%'} height="24"  alignItems='center' justify={'center'} as={'header'}> 
-            <Image src="images/logo.png" alt="worldtrip"/>  
+            <Image src="images/Logo.png" alt="worldtrip"/>  
         </Flex>
     )
 }
